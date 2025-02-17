57c634d1fe5d1dc42ef3f66a51741dfcc7117df3
kibumpark <top5w5@naver.com>
2025-02-17 16:04:39 +0900

    불필요 제거

--- for-model/frontend/src/App.vue
+++ for-model/frontend/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Default from '@/layouts/default.vue'
-import Chat from '@/components/Chat.vue'
 </script>
 
 <template>
   <VApp>
     <Default />
-    <Chat />
   </VApp>
 </template>
